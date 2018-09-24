02e34894798a152d723bf881dc80a0420aec08f6
anusha <anushareddy.g4@gmail.com>
2018-09-23 23:45:20 -0700

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GroceryComponent } from './grocery/grocery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, GroceryComponent]
 })
 export class AppModule { }
---
+++ src/app/grocery/grocery.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-grocery',
+  templateUrl: './grocery.component.html',
+  styleUrls: ['./grocery.component.css']
+})
+export class GroceryComponent implements OnInit {
+
+  task = {
+    name: '',
+    id: 0
+  };
+  tasks = [];
+
+
+  onClick() {
+    if (this.task.id === 0) {
+      this.tasks.push({id: (new Date()).getTime(), name: this.task.name, strike: false});
+    }
+    this.task = {
+      name: '',
+      id: 0
+    };
+  }
+  onEdit(item) {
+    this.task = item;
+  }
+
+  onDelete(item) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (item.id === this.tasks[i].id) {
+        this.tasks.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  onStrike(item) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (item.id === this.tasks[i].id) {
+        if (this.tasks[i].strike) {
+          this.tasks[i].strike = false;
+        } else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
